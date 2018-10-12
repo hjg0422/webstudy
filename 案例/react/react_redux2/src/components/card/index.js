e2d1f649856189e71d5c34172924c344443858a3
A Jun <39580138+hjg0422@users.noreply.github.com>
2018-10-12 12:04:39 +0800

    change

---
+++ 案例/react/react_redux2/src/components/card/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ImgInfo = (props)=>{
+    return <div>
+        <img src="" alt="用户头像"/>
+    </div>
+}
+export default class Card extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+
+        return <div className="user_card"></div>
+    }
+}
